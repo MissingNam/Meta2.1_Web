b4d2929708fad5fcbccf17067b16431f9cf28707
MissingNam <jhared.martinez@uabc.edu.mx>
2026-09-10 11:45:14 -0700

    Footer and Header añadido

---
+++ src/components/appFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-footer color="blue-lighten-4" class="pa-4">
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <strong>Nombre:</strong> Jhared Leonardo Martinez Garcia
+                </v-col>
+                <v-col cols="12" md="4">
+                    <strong>Matricula:</strong> 1197428
+                </v-col>
+                <v-col cols="12" md="4">
+                    <strong>Fecha:</strong> {{ fechaActual }}
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-footer>
+</template>
+
+<script setup>
+const fechaActual = new Date().toLocaleDateString('es-MX')
+</script>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app-bar color="primary">
+    <v-avatar class="ml-4">
+      <v-img src="https://picsum.photos/id/64/100/100"></v-img>
+    </v-avatar>
+    <v-app-bar-title>Mi Portafolio Fotográfico</v-app-bar-title>
+  </v-app-bar>
+</template>

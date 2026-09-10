be7c319325188c1112f31cf979bbf2b1b8069446
MissingNam <jhared.martinez@uabc.edu.mx>
2026-09-10 11:46:05 -0700

    Añadido Tarjeta de imagen y tabla de datos

---
+++ src/components/tablaDatos.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th>Habilidad</th>
+        <th>Nivel</th>
+        <th>Años de experiencia</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in habilidades" :key="item.nombre">
+        <td>{{ item.nombre }}</td>
+        <td>{{ item.nivel }}</td>
+        <td>{{ item.anios }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script setup>
+const habilidades = [
+  { nombre: 'Vue.js', nivel: 'Intermedio', anios: 1 },
+  { nombre: 'JavaScript', nivel: 'Intermedio', anios: 2 },
+  { nombre: 'HTML/CSS', nivel: 'Avanzado', anios: 2 },
+  { nombre: 'Git', nivel: 'Básico', anios: 1 },
+]
+</script>
---
+++ src/components/tarjetaImagen.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-img :scr="imagenUrl" heigth="200" cover></v-img>
+        <v-card-title>{{ titulo }}</v-card-title>
+        <v-card-subtitle>Foto por: {{ autor }}</v-card-subtitle>
+        <v-card-text>{{ descripcion }}</v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+defineProps({
+    imagenUrl: { type: String, required: true },
+    titulo: { type: String, default: 'Sin título' },
+    descripcion: { type: String, default: '' },
+    autor: { type: String, default: 'Desconocido' }
+})
+</script>
